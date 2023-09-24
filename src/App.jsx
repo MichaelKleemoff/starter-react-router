348182a6ac7f198f7cf48fce97d16978d06a174a
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-09-24 07:04:58 -0400

    First commit.

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,44 @@
-import Footer from "./components/common/footer.jsx";
-import Header from "./components/common/header.jsx";
-import Nav from "./components/common/nav.jsx";
-import About from "./components/pages/about.jsx";
-import Home from "./components/pages/home.jsx";
-import Newsletter from "./components/pages/newsletter.jsx";
-import ProductList from "./components/products/productList.jsx";
-import Product from "./components/products/product.jsx";
+import Footer from './components/common/footer.jsx';
+import Header from './components/common/header.jsx';
+import Nav from './components/common/nav.jsx';
+import About from './components/pages/about.jsx';
+import Home from './components/pages/home.jsx';
+import Newsletter from './components/pages/newsletter.jsx';
+import ProductList from './components/products/productList.jsx';
+import Product from './components/products/product.jsx';
 
-import lamps from "./data/lamps.json";
-import candles from "./data/candles.json";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import lamps from './data/lamps.json';
+import candles from './data/candles.json';
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <div className="wrapper">
-        <Nav />
-        <main>
-          <Home />
-          <About />
-          <Newsletter />
-          <ProductList products={lamps} type={"Lamps"} />
-          <ProductList products={candles} type={"Candles"} />
-        </main>
-      </div>
-      <Footer />
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Router>
+				<Header />
+				<div className='wrapper'>
+					<Nav />
+					<main>
+						<Routes>
+							<Route path='/' element={<Home />} />
+							<Route path='/about' element={<About />} />
+							<Route path='/newsletter' element={<Newsletter />} />
+							<Route
+								path='/lamps'
+								element={<ProductList products={lamps} type={'Lamps'} />}
+							/>
+							<Route
+								path='/candles'
+								element={<ProductList products={candles} type={'Candles'} />}
+							/>
+						</Routes>
+					</main>
+				</div>
+				<Footer />
+			</Router>
+		</div>
+	);
 }
 
 export default App;
